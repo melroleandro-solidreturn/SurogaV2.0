2af002f77516ca3ec7f7fcb13ca1277fe8ea7cac
leandro <zsio2@protonmail.me>
2026-01-27 23:52:25 +0000

    remove api

--- SurogaAPI/rides/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.db import transaction
-from django.utils.translation import gettext_lazy
-
-from core.models import User
-from rides.models import paymentlog
-
